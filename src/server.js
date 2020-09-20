26c867ce8e8f194a5849efa3fc1e21622019a2d0
Bruno Augusto <bruno.sidf@gmail.com>
2020-09-20 16:44:17 -0300

    'zenvia'

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const Router = require('express');
 const SpeechToTextService = require('./services/SpeechToTextService');
 const app = Router()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(Router.json());
 app.get('/', SpeechToTextService.get);
 app.post('/speech-to-text', SpeechToTextService.post);
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`started`);
 });
