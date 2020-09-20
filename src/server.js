7624e28fdc1b0ca9c87b3081cf9fd2cafc17ec1e
Bruno Augusto <bruno.sidf@gmail.com>
2020-09-20 17:35:26 -0300

    text to audio

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(Router.json());
 app.get('/', SpeechToTextService.get);
 app.post('/speech-to-text', SpeechToTextService.post);
+app.post('/text-to-speech', SpeechToTextService.textToSpeech);
 
 app.listen(port, () => {
   console.log(`started`);
--- src/services/SpeechToTextService.js
+++ src/services/SpeechToTextService.js
@@ -1,10 +1,8 @@
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
+const speech = require("@google-cloud/speech");
+const textToSpeech = require('@google-cloud/text-to-speech');
 
 class SpeechToText {
-
   async post(req, res) {
-    
     try {
       const client = new speech.SpeechClient();
 
@@ -18,7 +16,7 @@ class SpeechToText {
 
       if (!bytes || bytes.trim().length == 0) {
         return res.status(400).json({
-          result: 'Nenhum bytes informado',
+          result: "Nenhum bytes informado",
         });
       }
 
@@ -27,10 +25,10 @@ class SpeechToText {
       };
 
       const config = {
-        "encoding":"OGG_OPUS",
-          "sampleRateHertz": 16000,
-          "languageCode": "pt-BR",
-          "enableWordTimeOffsets": false
+        encoding: "OGG_OPUS",
+        sampleRateHertz: 16000,
+        languageCode: "pt-BR",
+        enableWordTimeOffsets: false,
       };
 
       const request = {
@@ -40,22 +38,64 @@ class SpeechToText {
 
       const [response] = await client.recognize(request);
 
-      const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
-      
+      const transcription = response.results
+        .map((result) => result.alternatives[0].transcript)
+        .join("\n");
+
       return res.json({
         transcription,
       });
     } catch (error) {
       return res.status(500).json({
-        error: 'Erro de interno.'
+        error: "Erro de interno.",
+      });
+    }
+  }
+
+  async textToSpeech(req, res) {
+    const { text } = req.body;
+
+    if (!text || text.trim().length == 0) {
+      return res.status(400).json({
+        result: "Texto inválido",
       });
     }
 
+    const client = new textToSpeech.TextToSpeechClient();
+
+    const audioConfig = {
+      audioEncoding: "OGG_OPUS",
+      pitch: 0,
+      speakingRate: 1,
+    };
+
+    const voice = {
+      languageCode: "pt-BR",
+      name: "pt-BR-Wavenet-A",
+    };
+
+    const input = {
+      text,
+    };
+
+    const request = {
+      audioConfig,
+      voice,
+      input,
+    };
+
+    // Performs the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    res.setHeader("Content-disposition", "attachment; filename=output.ogg");
+    res.setHeader("Content-type", "audio/ogg");
+    res.write(response.audioContent, "binary");
+    res.end();
   }
 
   async get(req, res) {
     return res.json({
-      result: 'Iniciado',
+      result: "Iniciado",
     });
   }
 }
